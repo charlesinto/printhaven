b1d5e6ff5720c30b01264a381cba36d2620624e9
Odeyemi Ibukun <odeyemiibukuna@gmail.com>
2021-09-23 11:38:18 +0100

    change environmenter variable

--- config/config.js
+++ config/config.js
@@ -6,20 +6,20 @@ module.exports = {
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
-    dialect: process.env.DIALECT
+    dialect: process.env.DB_DIALECT
   },
   test: {
     username: "root",
     password: null,
     database: "database_test",
     host: "127.0.0.1",
-    dialect: process.env.DIALECT
+    dialect:"mysql"
   },
   production: {
     username: process.env.DB_USERNAME_PROD,
     password: process.env.DB_PASSWORD_PROD,
     database: process.env.DB_DATABASE_PROD,
     host: process.env.DB_HOST_PROD,
-    dialect: process.env.DIALECT
+    dialect: process.env.DB_DIALECT_PROD
   },
 };
