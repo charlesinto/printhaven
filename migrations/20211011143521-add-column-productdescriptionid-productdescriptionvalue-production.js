41bd021750b1b441ab9dbef0fd969f809ada4280
charlesinto <charles.onuorah@yahoo.com>
2021-10-11 15:37:39 +0100

    user should be able to get product by ID

---
+++ migrations/20211011143521-add-column-productdescriptionid-productdescriptionvalue-production.js
@@ -0,0 +1,80 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.dropTable("ProductDesciptionValues");
+
+    await queryInterface.createTable("ProductDesciptionValues", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      productDesciptionId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          // User belongsTo ParentCategory 1:1
+          model: "ProductDesciptions",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable("ProductDesciptionValues");
+    await queryInterface.createTable("ProductDesciptionValues", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      descriptionId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          // User belongsTo ParentCategory 1:1
+          model: "ProductDesciptions",
+          key: "id",
+        },
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+};
